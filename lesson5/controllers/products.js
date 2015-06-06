e54980ee0717e524675c56f421c89949f47edc89
Hendrix Tavarez <htavarez@gmail.com>
2015-06-06 07:27:55 -0400

    Lesson5: Connecting and CRUD

--- lesson5/controllers/products.js
+++ lesson5/controllers/products.js
@@ -1,21 +1,55 @@
 var Product = require('../models/products');
 
 exports.getProducts = function (req, res) {
-	res.send("List of all products");
+	Product.find({}, function (err, products) {
+		res.json(products);
+	})
 };
 
 exports.getProduct = function (req, res) {
-	res.send("A single product");
+	Product.find({_id:req.params.productId}, function (err, product) {
+		if (err) {
+			res.send(err);
+		};
+		res.json(product);
+	})
 };
 
 exports.addProduct = function (req, res) {
-	res.send("product added");
+	var product = new Product();
+
+	product.name = req.body.productName;
+	product.description = req.body.productDescription;
+	product.price = req.body.productPrice;
+	product.photo = req.body.productPhotoUrl;
+
+	product.save(function (err) {
+		if (err) {
+			res.send(err);
+		};
+		res.send({message: "Product saved successfully."});
+	})
 };
 
 exports.updateProduct = function (req, res) {
-	res.send("product updated");
+	Product.update({_id:req.params.productId}, {
+		name: req.body.productName,
+		description: req.body.productDescription,
+		price: req.body.productPrice,
+		photo: req.body.productPhotoUrl
+	}, function (err, num, raw) {
+		if (err) {
+			res.send(err);
+		};
+		res.json(num);
+	})
 };
 
 exports.deleteProduct = function (req, res) {
-	res.send("product deleted");
+	Product.remove({_id:req.params.productId}, function (err) {
+		if (err) {
+			res.send(err);
+		};
+		res.json({message: "Product deleted successfully"});
+	})
 };
--- lesson5/server.js
+++ lesson5/server.js
@@ -2,14 +2,23 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var productController = require('./controllers/products');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/products");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
 app.use("/api", router);
 
 router.route("/products").get(productController.getProducts);
-router.route("/product").get(productController.getProduct);
+router.route("/product/:productId").get(productController.getProduct);
 router.route("/addProduct").post(productController.addProduct);
-router.route("/updateProduct").post(productController.updateProduct);
-router.route("/deleteProduct").get(productController.deleteProduct);
+router.route("/updateProduct/:productId").post(productController.updateProduct);
+router.route("/deleteProduct/:productId").get(productController.deleteProduct);
 
 var server = app.listen(3000, function () {
 	
